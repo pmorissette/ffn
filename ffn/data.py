34a0180a320e453013ff57d22671f992afde1c31
John Erik Sloper <john@quantfol.io>
2018-07-13 14:31:58 +0200

    Make ffn work with >0.23 version of pandas

--- ffn/data.py
+++ ffn/data.py
@@ -1,6 +1,15 @@
+from distutils.version import LooseVersion
+
+import pandas as pd
+
 import ffn
 import ffn.utils as utils
-import pandas as pd
+
+# This is a temporary fix until pandas_datareader 0.7 is released.
+# pandas 0.23 has moved is_list_like from common to api.types, hence the monkey patch
+if LooseVersion(pd.__version__) > LooseVersion('0.23.0'):
+    pd.core.common.is_list_like = pd.api.types.is_list_like
+
 from pandas_datareader import data as pdata
 
 
